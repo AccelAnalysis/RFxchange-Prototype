f8a994a0871d9ef3aff13aaeb42c759279284271
AccelAnalysis <jholman@accelanalysis.com>
2026-07-25 14:51:07 -0400

    Remove unused Vite config

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/RFxchange-Prototype/' : '/',
-  build: {
-    sourcemap: true,
-  },
-}));
